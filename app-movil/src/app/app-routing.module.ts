739c592170867b77dc4c700385c5778054d9c7d0
kevin jimenez <kivito2009@hotmail.com>
2019-01-27 12:26:25 -0500

    boton login ingreso a los tabs

--- app-movil/src/app/app-routing.module.ts
+++ app-movil/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: 'src/pages/login/login.module#LoginPageModule' },
-  { path: 'navegacion', loadChildren: 'src/pages/tabs/navegacion-tabs/navegacion-tabs.module#NavegacionTabsPageModule'},
+  {
+    path: '',
+    loadChildren: 'src/pages/login/login.module#LoginPageModule'
+  },
+  {
+    path: 'navegacion',
+    loadChildren: 'src/pages/tabs/navegacion-tabs/navegacion-tabs.module#NavegacionTabsPageModule'
+  },
 ];
 @NgModule({
   imports: [
